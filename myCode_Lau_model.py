56439f4f1bbe4eaa6d9eb09ac4aadc4cf4413896
Akimlav <58592031+Akimlav@users.noreply.github.com>
2023-01-16 14:57:01 +0100

    Add files via upload
    PC uni

--- myCode_Lau_model.py
+++ myCode_Lau_model.py
@@ -12,24 +12,25 @@ from scipy.signal import convolve #import convolve function from scipy.signal li
 import scipy.fftpack
 
 #Parameters values -- User inputs floats
-time = 1 #event duration, in seconds
-# time2 = 3
+time = 350 #event duration, in seconds
+t_inj = 0.4 #inhection duration, in seconds
 delta_t = 0.1 #(s) time-steps
-# delta_t2 = 1 #(s) time-steps
-# time2 = 5
-l = 2 #x-length (m) of room
-w = 2 #y-length (m) of room
-h = 2 #z-length (m) of room
-x_o = 1 #x-coordinate of source
-y_o = 1 #y-coordinate of source
-v = 1 #air velocity (m/s) from left to right. 
-R = 1 #aerosol emission rate (particles/s)
+
+delta_x = 0.05  #(m) mesh-size
+
+l = 3.14 #x-length (m) of room
+w = 3.14 #y-length (m) of room
+h = 3.14 #z-length (m) of room
+
+x_o = 1.57 #x-coordinate of source
+y_o = 1.57 #y-coordinate of source
+
+v = 0.2 #air velocity (m/s) from left to right. 
+R = 100 #aerosol emission rate (particles/s)
 Q = 0 #0.002 # Air exchange rate (s^-1)
 K = 5e-3 #0.0053 # Eddy diffusion coefficient (m^2/s)
 d = 0 #1.7*10**(-4) #deactivation rate (s^-1)
 s = 0 # 1.1*10**(-4) #settling rate (s^-1)
-delta_x = 0.025  #(m) mesh-size
-
 
 #set up mesh
 n_x = int(l / delta_x) + 1 #int: calculate number of x-steps
@@ -39,25 +40,17 @@ y = np.linspace(0,w,n_y) #define numpy array for y-axis
 X,Y = np.meshgrid(x,y) #define numpy meshgrid for X,Y
  #Initialise numpy array of same size as X for C (the concentration)
 
-# timelist = [time, time2]
 reslist = []
-# deltaTlist = [delta_t, delta_t2]
-vmax = 30
+vmax = 20
 levels = np.linspace(0, vmax, n_x+1)
 
-
-# for tt in range(len(timelist)):
-C = np.zeros_like(X)
 term1temp = []
 term2temp = []
 term3temp = []
-#time-axis
 t_end = time
 n_t = int(t_end/delta_t)
 t_arr = np.linspace(delta_t,t_end,n_t)
-# t_arr = t_arr[4:]
-# S = delta_t * np.full(len(t_arr), R)
-m = 100 #int(v/(2*l) *time) 
+m = 20 #int(v/(2*l) *time) 
 
 for t in range(0,len(t_arr)):
     print('_____________________________________________________________')
@@ -87,18 +80,16 @@ for t in range(0,len(t_arr)):
     term1tempArr[np.isnan(term1tempArr)] = 0
     term2tempArr[np.isnan(term2tempArr)] = 0
 
-
     S = np.full(np.shape(term1temp)[0], R)*delta_t
-    newS = np.ones(len(S))
-    newS[:4] = R*delta_t
+    # S = np.full(np.shape(term1temp)[0], R)
     C = np.zeros_like(X)
-    CC = np.zeros_like(X)
     integlist = []
     
     for i in range(len(x)):
         for j in range(len(y)):
             integ = 1/(4*np.pi*K*t_arr[:t+1]) * term1tempArr[0,i,:] * term2tempArr[j,0,:] * term3temp[:t+1]
             C[j][i] = convolve(S,integ,mode='valid') / (h/2)
+            
             # r = scipy.fftpack.fft(S).real * scipy.fftpack.fft(integ).real
             # rr = scipy.fftpack.ifft(r).real
             # print(rr)
@@ -111,30 +102,45 @@ for t in range(0,len(t_arr)):
     reslist.append(C)
     cc = np.unravel_index(C.argmax(), C.shape)
     ccc = np.subtract(np.asarray(t1.shape),1)/2
-
-    # print(t - 3)
-    if t_arr[t] > 0.4:
-        print(t - 3)
+    
+    
+    if t_arr[t] > t_inj:
+        itemindex = np.where(t_arr == t_inj+delta_t)
+        CC = np.subtract(reslist[t],reslist[t-itemindex[0][0]])
+        cc_max = np.unravel_index(CC.argmax(), CC.shape)
         
-        CCC = np.subtract(reslist[t],reslist[t-3])
-
         fig,ax = plt.subplots(1,1, figsize=(5.5, 5))
-        ax.set_title('modified')
-        cp = ax.contourf(X, Y, CCC, levels=levels, vmin=0, vmax=vmax)
+        tt = np.round(t_arr[t],3)
+        ax.set_title('Concentration ', 'V: ' + str(v) + ', time: ' +  str(tt) + ' s')
+        cp = ax.contourf(X, Y, CC, levels=levels, vmin=0, vmax=vmax)
         plt.colorbar(cp)
-        fig.tight_layout()
         plt.axis('square')
+        plt.ylabel('y, m')
+        plt.xlabel('x, m')
+        fig.tight_layout()
+        plt.savefig('./conc_' + str(tt) + '.png', dpi = 100)
         plt.show()
-        print('ps', sum(sum(CCC)), '|', CCC[int(ccc[0]), int(ccc[1])],'|', 
-                'max value: ',cc, CCC[cc[0]][cc[1]])
+        
+        print('ps', sum(sum(CC)),'|', 'max value: ',cc_max, CC[cc_max[0]][cc_max[1]])
+        diff0 = abs(CC[cc_max[0]][cc_max[1]] - CC[0,0])
+        diff1 = abs(CC[cc_max[0]][cc_max[1]] - CC[0,np.asarray(t1.shape)[0]-1])
+        diff2 = abs(CC[cc_max[0]][cc_max[1]] - CC[np.asarray(t1.shape)[0]-1,0])
+        diff3 = abs(CC[cc_max[0]][cc_max[1]] - CC[np.asarray(t1.shape)[0]-1, np.asarray(t1.shape)[0]-1])
+        if diff0 and diff1 and diff2 and diff3 < 1e-3:
+            print('Converged!')
+            print(diff0, diff1, diff2, diff3)
+            break
     else:
         fig,ax = plt.subplots(1,1, figsize=(5.5, 5))
-        ax.set_title('Normal')
+        tt = np.round(t_arr[t],3)
+        ax.set_title('Concentration ', 'V: ' + str(v) + ', time: ' +  str(tt) + ' s')
         cp = ax.contourf(X, Y, C, levels=levels, vmin=0, vmax=vmax)
         plt.colorbar(cp)
+        plt.axis('square')
+        plt.ylabel('y, m')
+        plt.xlabel('x, m')
         fig.tight_layout()
-        plt.axis('square') 
+        plt.savefig('./conc_' + str(tt) + '.png', dpi = 100)
         plt.show()
         print('ps', sum(sum(C)), '|', C[int(ccc[0]), int(ccc[1])],'|', 
               'max value: ',cc, C[cc[0]][cc[1]])
-    
